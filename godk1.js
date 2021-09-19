26ea35ccb90f048489482e270c40a38e8ef160b9
pernillehopfner <90635224+pernillehopfner@users.noreply.github.com>
2021-09-19 13:05:20 +0200

    start på godkendelsesopgave

---
+++ godk1.js
@@ -0,0 +1,5 @@
+let hemmeligBesked = ['hej','klaus','vil','du','med','til','fodbold','i','klubben','i','aften','?','kurt']
+
+//1. Brug en array metode til at slette det sidste element af arrayet hemmeligBesked
+hemmeligBesked.pop()
+console.log(hemmeligBesked); 
